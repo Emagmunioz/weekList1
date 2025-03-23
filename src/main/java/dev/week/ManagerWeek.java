d219e36858f7c261cbebfd1f02fc160c4e539b3e
Emagmunioz <emagmunioz@gmail.com>
2025-03-23 22:45:39 +0100

    Add testCreateDaysOfWeek to verify the creation of the weekdays list.

--- src/main/java/dev/week/ManagerWeek.java
+++ src/main/java/dev/week/ManagerWeek.java
@@ -1,21 +1,17 @@
 package dev.week;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-public class ManagerWeek() private ArrayList weekdays;
 
-{
-    this.weekdays = new ArrayList<>();
-    initializeWeekdays();
-}
+public class ManagerWeek {
+    private List<String> daysOfWeek;
 
-private void initializeWeekdays() {
-    weekdays.add("Monday");
-    weekdays.add("Tuesday");
-    weekdays.add("Wednesday");
-    weekdays.add("Thursday");
-    weekdays.add("Friday");
-    weekdays.add("Saturday");
-    weekdays.add("Sunday");
+    public ManagerWeek() {
+        this.daysOfWeek = new ArrayList<>();
+    }
+
+    
+
+
+   
 
 }
--- src/test/java/ManagerWeekTest.java
+++ src/test/java/ManagerWeekTest.java
@@ -1,6 +1,6 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.Before;
-import org.junit.Test;
+import dev.week.ManagerWeek;
 import java.util.Arrays;
 import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,10 +8,15 @@ import static org.hamcrest.Matchers.*;
 
 public class ManagerWeekTest {
     private ManagerWeek managerWeek;
-    @Before
+    @BeforeAll
     public void setUp() {
         managerWeek = new ManagerWeek();
         managerWeek.createDaysOfWeek();
     }
-  
+
+   @Test
+    public void testCreateDaysOfWeek() {
+        List<String> expectedDays = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+        assertThat(managerWeek.getDaysOfWeek(), is(expectedDays));
+    }
 }
