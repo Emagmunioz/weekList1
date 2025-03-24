431a2cb7d45b4125dcb023fc0b02b6e4e3b39d0c
Emagmunioz <emagmunioz@gmail.com>
2025-03-24 10:34:57 +0100

    The test for crearListaDias now passes.

---
+++ src/main/java/dev/week/Semana.java
@@ -0,0 +1,25 @@
+package dev.week;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Semana {
+    private List<String> diasSemana;
+    public Semana() {
+        this.diasSemana = new ArrayList<>();
+    }
+    public void crearListaDias() {
+        diasSemana.add("Lunes");
+        diasSemana.add("Martes");
+        diasSemana.add("Miercoles");
+        diasSemana.add("Jueves");
+        diasSemana.add("Viernes");
+        diasSemana.add("sabado");
+        diasSemana.add("domingo");
+    }
+
+
+
+    }
+
+
--- src/test/java/semanaTest.java
+++ src/test/java/semanaTest.java
@@ -1,14 +1,15 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import dev.week.Semana;
 import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class semanaTest {
-    private semana gestor;
+    private Semana gestor;
     @BeforeEach
     public void setUp() {
-        gestor = new semana();
+        gestor = new Semana();
         gestor.crearListaDias();
     }
 
