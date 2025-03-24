48f2bd8610ee6caf59e5f7f5c8b0f69e3449794a
Emagmunioz <emagmunioz@gmail.com>
2025-03-24 09:50:25 +0100

    Added a failing test for the crearListaDias method.

---
+++ src/test/java/semanaTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class semanaTest {
+    private semana
+
+}
