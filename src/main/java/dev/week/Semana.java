3c47eb60c6e8c0d2ee3aae8065428a81badffa8b
Emagmunioz <emagmunioz@gmail.com>
2025-03-24 11:20:55 +0100

    Implemented the crearListaDias method in the semana class and  test for crearListaDias now passes

--- src/main/java/dev/week/Semana.java
+++ src/main/java/dev/week/Semana.java
@@ -4,6 +4,24 @@ import java.util.Collections;
 import java.util.List;
 
 public class Semana {
+    private List<String> diasSemana;
+
+    public Semana() {
+        this.diasSemana = new ArrayList<>();
+    }
+    public void crearListaDias() {
+        this.diasSemana.add("Lunes");
+        this.diasSemana.add("Martes");
+        this.diasSemana.add("Miercoles");
+        this.diasSemana.add("Jueves");
+        this.diasSemana.add("Viernes");
+        this.diasSemana.add("Sabado");
+        this.diasSemana.add("Domingo");
+    }
+    public List<String> obtenerDiasSemana() {
+        return this.diasSemana;
+    }
+    
     
 
 
