835a04b5dabf31b142f1dc0e20c59c74fab272e5
Emagmunioz <emagmunioz@gmail.com>
2025-03-24 11:09:08 +0100

    delete method crearListaDias

--- src/main/java/dev/week/Semana.java
+++ src/main/java/dev/week/Semana.java
@@ -4,19 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Semana {
-    private List<String> diasSemana;
-    public Semana() {
-        this.diasSemana = new ArrayList<>();
-    }
-    public void crearListaDias() {
-        diasSemana.add("Lunes");
-        diasSemana.add("Martes");
-        diasSemana.add("Miercoles");
-        diasSemana.add("Jueves");
-        diasSemana.add("Viernes");
-        diasSemana.add("sabado");
-        diasSemana.add("domingo");
-    }
+    
 
 
 
